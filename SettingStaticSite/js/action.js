9dcda4e4c9d8c08a6eb32ab97a0dc774b2deb8df
Navigator <yuanyedmc@hotmail.com>
2023-12-14 15:50:45 +0800

    improve the js interaction

--- SettingStaticSite/js/action.js
+++ SettingStaticSite/js/action.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', function() {
+  alert('欢迎访问个人简介页面！');
+});
+
+
 const toggleButton = document.querySelector('.toggle-button');
 const moreContent = document.querySelector('.more-content');
 
@@ -5,7 +10,9 @@ toggleButton.addEventListener('click', function() {
   moreContent.classList.toggle('show-more');
   if (moreContent.classList.contains('show-more')) {
     toggleButton.textContent = '隐藏';
+    moreContent.style.display = 'block';
   } else {
     toggleButton.textContent = '展示图片';
+    moreContent.style.display = 'none';
   }
 });
