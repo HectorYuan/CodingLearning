f9d20e5c2eebef9078f5ad6946c21ff336fda663
Navigator <yuanyedmc@hotmail.com>
2023-12-15 01:46:39 +0800

    using iframe displays navibar

--- SettingStaticSite/js/action.js
+++ SettingStaticSite/js/action.js
@@ -1,26 +1,23 @@
+/*
 window.addEventListener('load', function() {
-  alert('欢迎访问个人简介页面！');
+  alert('欢迎访问我的网站！');
 });
+*/
 
+// 管理图片控制按钮
+const toggleButtons = document.querySelectorAll(".toggle-button");
 
-const toggleButtons = document.querySelectorAll('.toggle-button');
+toggleButtons.forEach(function (toggleButton) {
+  toggleButton.addEventListener("click", function () {
+    const moreContent = this.parentElement.querySelector(".more-content");
+    moreContent.classList.toggle("show-more");
 
-
-
-toggleButtons.forEach(function(toggleButton) {
-  toggleButton.addEventListener('click', function() {
-    const moreContent = this.parentElement.querySelector('.more-content');
-    moreContent.classList.toggle('show-more');
-
-    if (moreContent.classList.contains('show-more')) {
-      toggleButton.textContent = '隐藏';
-      moreContent.style.display = 'block';
+    if (moreContent.classList.contains("show-more")) {
+      toggleButton.textContent = "隐藏";
+      moreContent.style.display = "block";
     } else {
-      toggleButton.textContent = '展示图片';
-      moreContent.style.display = 'none';
+      toggleButton.textContent = "展示图片";
+      moreContent.style.display = "none";
     }
   });
-  
 });
-
-
